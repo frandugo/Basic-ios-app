110a34db5a776dfb6765ce55407a3f54b9efe01c
Andres <amontes@joonik.com>
2019-08-06 18:17:28 -0500

    first commit

--- RadomFacts/ViewController.swift
+++ RadomFacts/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var radomFactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func showAnotherFunFact() {
+        print("Hello pacho")
+        radomFactLabel.text = "This is a fun fact!"
+    }
+    
 }
 
