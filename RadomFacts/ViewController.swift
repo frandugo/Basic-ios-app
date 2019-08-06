e756878bf139f9f111985dc28e319ae1cb68ea8c
Andres <amontes@joonik.com>
2019-08-06 14:55:29 -0500

    Initial Commit

---
+++ RadomFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RadomFacts
+//
+//  Created by user on 8/6/19.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
